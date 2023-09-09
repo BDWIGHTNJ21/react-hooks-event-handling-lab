bccd3c3157f9fb41a1548b8f3477657ea0499d0b
Bernice Dwight <BDWIGHTNJ21@GMAIL.COM>
2023-09-09 13:44:43 -0400

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe (){
+const Focus =() =>{ 
+console.log('Good!');   
+
+};
+    const Blur =()=>{
+         console.log('Hey! Eyes on me!');
+    };
+         
+       return (
+       <button onFocus={Focus}onBlur={Blur}>'Eyes on me' </button>
+   
+       );  
+  
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+
+
+ function Keypad(){
+const handleChange=(event)=> {
+    console.log('Entering password...',event.target.value);
+};
+return (
+
+        <div>
+          <input type="password" onChange={handleChange}/>
+        </div>
+    );
+   
 }
 
 export default Keypad;
